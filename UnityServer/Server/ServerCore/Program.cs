8258d9835676aaf09309ff2c79fb3e386efca6db
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-04-10 17:42:39 +0900

    multi-thread

--- UnityServer/Server/ServerCore/Program.cs
+++ UnityServer/Server/ServerCore/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Server
 {
     class Program
     {
+        static void MainThread(object state)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello Thread");
+            } 
+              
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task t = new Task(() => { while (true) { } },TaskCreationOptions.LongRunning);
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            while (true)
+            {
+
+            }
+
+            //Thread t = new Thread(MainThread); // default thread is t.IsBackGround is false
+            //t.IsBackground = true;
+            //t.Name = "Test Thread";
+            //t.Start();
+            //Console.WriteLine("Waiting for Thread");
+            //t.Join();
+
+            //Console.WriteLine("Hello, World!");
+
+
         }
     }
 }
